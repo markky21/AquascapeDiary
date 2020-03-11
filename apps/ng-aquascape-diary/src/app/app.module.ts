4a5b9eccac3a404d072bb0f5b82cfad2bafb1889
Marcin Mirecki <markky21@gmail.com>
2020-03-11 22:37:27 +0100

    Added PWA configuration

--- apps/ng-aquascape-diary/src/app/app.module.ts
+++ apps/ng-aquascape-diary/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
@@ -12,6 +12,8 @@ import {
   NbWindowModule
 } from '@nebular/theme';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -36,7 +38,8 @@ import { AppComponent } from './app.component';
     NbChatModule.forRoot({
       messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY'
     }),
-    CoreModule
+    CoreModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) as ModuleWithProviders<{}>
   ],
   bootstrap: [AppComponent]
 })
